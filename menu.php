8b141dc4b61597496f6e8e8e84b1d7518bbbe9be
Patricio Leon <patricio.lyono@gmail.com>
2023-08-21 20:39:34 -0400

    dropdown

--- menu.php
+++ menu.php
@@ -100,6 +100,7 @@ document.oncontextmenu = function(){return false}
               <li><button type="button" class="accordion-content" onclick="window.open('mod_repuestos/mdr.php','centro');" title="mdr.php?tipo=<?php echo $tipousuario?>" value="Estructura" name="Estructura" > Repuestos Médicos </button></li>
           </ul>
       </li>
+
            <li>
               <div class="link"><i class="icon-folder-open"></i> Administración <i class="icon-down-open"></i></div>
               <ul class="submenu">
@@ -108,7 +109,7 @@ document.oncontextmenu = function(){return false}
                   <li><button type="button" class="accordion-content" onclick="window.open('administrar_usuarios.php','centro');" value="Administrar Usuarios" name="Administrar Usuarios" > Administrar Usuarios </button></li>
               </ul>
            </li>
-      </li>
+
       <li>
           <div class="link"><i class="icon-folder-open"></i> Servicios generales <i class="icon-down-open"></i></div>
           <ul class="submenu">
