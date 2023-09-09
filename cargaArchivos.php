d5557c7e2df86b97eb33600d72d5736a005f263e
Patricio Leon Ormazabal <74997898+Patricioleono@users.noreply.github.com>
2023-09-09 01:02:09 -0300

    Create cargaArchivos.php

---
+++ cargaArchivos.php
@@ -0,0 +1,27 @@
+<?php
+//SELECTOR TIPO DE EQUIPO EI ETC.
+//SELECTOR DE ID STL - CONSULTA BDO PARA OBTENER IDENTIFICADORES
+//SELECTOR CAJA O EQUIPO
+//SUBIR N CANTIDAD DE STL
+//SUBIR EXCEL E INSERTAR DATOS EN BDO LOS DATOS TIENEN QUE TENER COMO IDENTIFICADOR AL EQUIPO QUE CORRESPONDEN
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Sistema Hospital</title>
+</head>
+<body>
+<h1>Hello, world!</h1>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
