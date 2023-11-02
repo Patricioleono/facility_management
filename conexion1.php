baff2418e29a3c1e04f9a7a21f6258a3aaba446e
Patricio Leon Ormazabal <74997898+Patricioleono@users.noreply.github.com>
2023-11-01 22:18:38 -0300

    Update conexion1.php

--- conexion1.php
+++ conexion1.php
@@ -1,8 +1,8 @@
 <?php 
 
  define('DB_SERVER','localhost:3306');
- define('DB_NAME','bimcl_calvo2017');
- define('DB_USER','bimcl_cc');
+ define('DB_NAME','bimcl_cc');
+ define('DB_USER','bimcl_calvo2017');
  define('DB_PASS','bimcalvo2017');
 
 
